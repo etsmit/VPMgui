49f209d19c2b4ef80ba7843ffc5a74ce0a1f8983
etsmit <etsmit12@gmail.com>
2024-06-24 12:27:45 -0400

    prototype done

--- VPM_Gui_evan.py
+++ VPM_Gui_evan.py
@@ -26,17 +26,6 @@ Rcvr_Dict = {"PF342 (Rcvr_342)": "Rcvr_342", "PF800 (Rcvr_800)": "Rcvr_800", "L-
 #keys are formatted {mode}/{bw}
 Nchan_Grayout_Scheme = {'coherent/100' : np.r_[0,5:9], 'coherent/200' : np.r_[0,6:9], 'coherent/800' : np.r_[8], 'coherent/1500' : np.r_[8], 'incoherent/100' : np.r_[0:4,8], 'incoherent/200' : np.r_[0:5], 'incoherent/800' : np.array([]), 'incoherent/1500' : np.r_[8]}
 
-#should try nested dictionaries here lol
-Scale_Scheme = {
-    'coherent/100/64':820, 'coherent/100/128':595, 'coherent/100/256':1650, 'coherent/100/512':2355,
-    'coherent/200/64':605, 'coherent/200/128':865, 'coherent/200/256':620, 'coherent/200/512':1720, 'coherent/200/1024':2430,
-    'coherent/800/32':375, 'coherent/800/64':420, 'coherent/800/128':800, 'coherent/800/256':940, 'coherent/800/512':1585, 'coherent/800/1024':880,  'coherent/800/2048':3155, 'coherent/800/4096':4550,
-    'coherent/1500/32':365, 'coherent/1500/64':530, 'coherent/1500/128':730, 'coherent/1500/256':1070, 'coherent/1500/512':1450, 'coherent/1500/1024':1085,  'coherent/1500/2048':3000, 'coherent/1500/4096':3750,
-    'incoherent/100/512':1875, 'incoherent/100/1024':4010, 'incoherent/100/2048':550, 'incoherent/100/4096':990, 'incoherent/100/8192':580,
-    'incoherent/200/1024':1920, 'incoherent/200/2048':1030, 'incoherent/200/4096':540, 'incoherent/200/8192':1045, 
-    'incoherent/800/32':14380, 'incoherent/800/64':7240, 'incoherent/800/128':14690, 'incoherent/800/256':7340, 'incoherent/800/512':15320, 'incoherent/800/1024':7495, 'incoherent/800/2048':14300, 'incoherent/800/4096':7545, 'incoherent/800/8192':24725,
-    'incoherent/1500/32':14675, 'incoherent/1500/64':6835, 'incoherent/1500/128':13485, 'incoherent/1500/256':6750, 'incoherent/1500/512':13345, 'incoherent/1500/1024':6655, 'incoherent/1500/2048':13035, 'incoherent/1500/4096':6595, 'incoherent/1500/8192':13500,
-    }
 
 Scale_Scheme = {
     'coherent' : {
@@ -50,7 +39,7 @@ Scale_Scheme = {
             '32':375, '64':420, '128':800, '256':940, '512':1585, '1024':880, '2048':3155, '4096':4550,
             },
         '1500' : {
-            '32':365, '64':530, '128':730, '256':1070, '512':1450, '1024':1085, '2048':300, '4096':3750,
+            '32':365, '64':530, '128':730, '256':1070, '512':1450, '1024':1085, '2048':3000, '4096':3750,
             },
         },
     'incoherent' : {
@@ -70,9 +59,6 @@ Scale_Scheme = {
     }
 
 
-
-
-
 #conversion gui -> internal variable names
 Diode_Scheme = {'Low Power':"'lo'", 'High Power':"'hi'", 'Off':"'off'"}
 Stokes_Scheme= {'Total Intensity':"'total_intensity'", 'Full Stokes':"'full_stokes'"}
@@ -82,11 +68,11 @@ Stokes_Scheme= {'Total Intensity':"'total_intensity'", 'Full Stokes':"'full_stok
 Rcvr_specific = {
     'Rcvr_342' : [1, 350, 100, 0, 350, 0.0],
     'Rcvr_800' : [1, 820, 200, 1, 820, 0.0],
-    'Rcvr1_2'  : [1, 1500, 800, 2, 1500, 0.0],
+    'Rcvr1_2'  : [1, 1500, 800, 2, 1500, 0.0, 'In'],
     'Rcvr2_3'  : [1, 2165, 1500, 3, 2165, 0.0],
     'Rcvr4_6'  : [4, """[{'bank':'A','restfreq':7687.5},\n\t{'bank':'B','restfreq':6562.5},\n\t{'bank':'C','restfreq':5437.5},\n\t{'bank':'D','restfreq':4312.5}]""", 1500, 3, 5637.5, "0.0,0.0,0.0,0.0"],
     'Rcvr8_10' : [4, """[{'bank':'A','restfreq':11506.8359375},\n\t{'bank':'B','restfreq':10504.8828125},\n\t{'bank':'C','restfreq':9502.9296875},\n\t{'bank':'D','restfreq':8500.9765625}]""", 1500, 3, 9702.9296875, "0.0,0.0,0.0,0.0"],
-    'Rcvr_2500': [3, "1225.0,2350.0,3475.0", 1500, 3, 6875.0, 0.0],
+    'Rcvr_2500': [3, "1225.0, 2350.0, 3475.0", 1500, 3, 6875.0, 0.0],
     }
 
 
@@ -110,25 +96,40 @@ class App(QMainWindow, Ui_MainWindow):
         self.Observation_Mode.activated.connect(self.DisEnable_Fold_Options)
         self.Dispersion_Mode.activated.connect(self.DisEnable_Fold_Options)
 
+        #things to default on start
+        default_grayout = Nchan_Grayout_Scheme['coherent/100']
+        for i in default_grayout:
+                    self.Nchan.model().item(i).setEnabled(False)
+        self.Nchan.setCurrentIndex(1)
+        self.Receiver_Names.setCurrentIndex(2)
+        self.Parfile.setEnabled(False)
+        rx = 'Rcvr1_2'
+        self.RestFrequency.setText(str(Rcvr_specific[rx][1]))
+        self.Band_Width.setCurrentIndex(Rcvr_specific[rx][3])
+        self.DopplerTrackFreq.insert(str(Rcvr_specific[rx][4]))
+        self.DeltaFreq.insert(str(Rcvr_specific[rx][5]))
+        self.Fold_Bins.insert('2048')
+        self.Fold_Time.insert('10.0')
+
     #Gray out nchan values based on the coherent/incoherent and bandwaidth selected
     def Combo_Box_Bw_Nchan(self):
         Count_Nchan = self.Nchan.count()
         for contents in range(Count_Nchan):
             self.Nchan.model().item(contents).setEnabled(True)
-            self.Tint.model().item(contents).setEnabled(True)
+            #self.Tint.model().item(contents).setEnabled(True)
 
         def Gray_Out_Bw_Nchan(grayout_index,Band_Width_Item):
             Band_Width_Selected_Item  = self.Band_Width.currentText()
 
             if Band_Width_Selected_Item == Band_Width_Item:
                 self.Nchan.model().item(1).setEnabled(False)
-                self.Tint.model().item(1).setEnabled(False)
+                #self.Tint.model().item(1).setEnabled(False)
 
                 for i in range(9):
                     self.Nchan.model().item(i).setEnabled(True)
                 for i in grayout_index:
                     self.Nchan.model().item(i).setEnabled(False)
-                    self.Tint.model().item(i).setEnabled(False)
+                    #self.Tint.model().item(i).setEnabled(False)
 
         current_mode = f'{self.Dispersion_Mode.currentText()}/{self.Band_Width.currentText()}'
         
@@ -170,11 +171,11 @@ class App(QMainWindow, Ui_MainWindow):
         mode = self.Dispersion_Mode.currentText()
         nchan = self.Nchan.currentText()
         
-        key = f'{mode}/{bw}/{nchan}'
-        #scale = Scale_Scheme[key]
         scale = Scale_Scheme[mode][bw][nchan]
+
+        print(float(nchan), (float(bw)*1e6))
     
-        tint = 16 * float(nchan) / float(bw)
+        tint = f'{self.Accum_Len.currentText()}*nchan / bw)'
 
         return tint,scale
 
@@ -202,8 +203,9 @@ class App(QMainWindow, Ui_MainWindow):
         nchan   = self.Nchan.currentText()
         nwin    = self.Num_Win.currentText()
         obsmode = self.Observation_Mode.currentText()
-        Config_Name = f"config_vpm_{rcvr}_{ddmode[0]}{bw}x{nchan}_{obsmode}".format(Rcvr_Dict[rcvr],ddmode,bw,nchan,nwin,obsmode)
-        self.Display_Script.append(Config_Name)
+        Config_Name = f"config_vpm_{rcvr}_{ddmode[0]}{bw}x{nchan}_{obsmode}"
+        #self.Display_Script.append(Config_Name)
+        return Config_Name
 
     #make the configuration dictionary
     #need to make new function that turns the dictionary into a string,
@@ -213,7 +215,9 @@ class App(QMainWindow, Ui_MainWindow):
         Config_dict = {}
         #set obvious things
         Config_dict['obstype'] = "'Pulsar'"
+        Config_dict['backend'] = "'VEGAS'"
         Config_dict['vframe'] = "'topo'"
+        Config_dict['vdef'] = "'Radio'"
         Config_dict['vlow'] = "0.0"
         Config_dict['vhigh'] = "0.0"
         Config_dict['vegas.outbits'] = "8"
@@ -232,7 +236,7 @@ class App(QMainWindow, Ui_MainWindow):
         if self.Dispersion_Mode.currentText() == 'incoherent':
             obsmode = f'{self.Observation_Mode.currentText}'
         else:
-            obsmode = f'{self.Dispersion_Mode.currentText()}_{self.Observation_Mode.currentText}'
+            obsmode = f'{self.Dispersion_Mode.currentText()}_{self.Observation_Mode.currentText()}'
         Config_dict['vegas.obsmode'] = obsmode
 
         #obsmode specific things
@@ -250,16 +254,24 @@ class App(QMainWindow, Ui_MainWindow):
 
 
         #check for fold/dm parameters here
-        if obsmode == 'fold':
-            valid_par = os.path.isfile(self.Parfile.currentText())
+        if obsmode[-4:] == 'fold':
+            valid_par = os.path.isfile(self.Parfile.text())
+            print('Valid parfile:',valid_par)
+        else:
+               valid_par = True
+        Config_dict['vegas.fold_parfile'] = self.Parfile.text()
 
         #warn user if they intend to leave dm=0/blank for coh dd
         ddmode = self.Dispersion_Mode.currentText()
         dm = self.Dispersion_Measure.text()
-        if (ddmode == 'incoherent') and (dm == '0' or dm == ''):
+        if dm =='':
+            dm = '0'
+        if (ddmode == 'coherent') and (dm == '0'):
             valid_dm = False
         else:
             valid_dm = True
+        print('Valid DM',valid_dm)
+        Config_dict['vegas.dm'] = self.Dispersion_Measure.text()
 
 
 
@@ -272,7 +284,7 @@ class App(QMainWindow, Ui_MainWindow):
         #unpickable things
         tint,scale = self.Auto_Select_Tint_and_Scale()
         Config_dict['vegas.scale'] = scale
-        Config_dict['tint'] = f'{tint*1e6}e-6'
+        Config_dict['tint'] = tint#f'{tint*1e6}e-6'
 
 
         #Rx specific things
@@ -280,18 +292,67 @@ class App(QMainWindow, Ui_MainWindow):
         #these are grabbed from the gui, which are set automatically via Auto_Select_Rx_specifics()
         Config_dict['nwin'] = self.Num_Win.currentText()
         Config_dict['restfreq'] = self.RestFrequency.toPlainText()
-        print(Config_dict['restfreq'])
         Config_dict['bandwidth'] = self.Band_Width.currentText()
         Config_dict['dopplertrackfreq'] = self.DopplerTrackFreq.text()
         Config_dict['deltafreq'] = self.DeltaFreq.text()
+        if rx == 'Rcvr1_2':
+            Config_dict['notchfilter'] = "'In'"
         
         print(Config_dict)
 
+        for key in Config_dict.keys():
+            print(f'{key} = {Config_dict[key]}')
+
         #now print the things to the display
         
-        #output_str = """"""
+        output_str = """"""
 
+        output_str += f'{time.time()}\n'
+
+        #errors first
+        if not valid_par:
+            output_str += 'ERROR: Invalid Parfile. Please double check your filename.\n'
         
+
+        #warnings:
+        if not valid_dm:
+            output_str +=  'WARNING: DM set to 0 or blank. Did you intend to leave that as is?\n'
+
+        #now config statements
+        output_str += '========================================\n'
+
+        output_str += f'{self.Create_Name()} = """\n'
+        
+        output_str += f"obstype = {Config_dict['obstype']}\n"
+        output_str += f"backend = {Config_dict['backend']}\n"
+        output_str += f"receiver = {Config_dict['receiver']}\n"
+        if Config_dict['receiver'] == 'Rcvr1_2':
+            output_str += f"notchfilter = {Config_dict['notchfilter']}\n"
+        output_str += f"restfreq = {Config_dict['restfreq']}\n"
+        output_str += f"nwin = {Config_dict['nwin']}\n"
+        output_str += f"deltafreq = {Config_dict['deltafreq']}\n"
+        output_str += f"swtype = {Config_dict['swtype']}\n"
+        output_str += f"swper = {Config_dict['swper']}\n"
+        output_str += f"swfreq = {Config_dict['swfreq']}\n"
+        output_str += f"vlow = {Config_dict['vlow']}\n"
+        output_str += f"vhigh = {Config_dict['vhigh']}\n"
+        output_str += f"vframe = {Config_dict['vframe']}\n"
+        output_str += f"vdef = {Config_dict['vdef']}\n"
+        output_str += f"vegas.polnmode = {Config_dict['vegas.polnmode']}\n"
+        output_str += f"vegas.numchan = {Config_dict['vegas.numchan']}\n"
+        output_str += f"vegas.outbits = {Config_dict['vegas.outbits']}\n"
+        output_str += f"vegas.scale = {Config_dict['vegas.scale']}\n"
+        if Config_dict['vegas.obsmode'][-4:] == 'fold':
+            output_str += f"vegas.fold_dumptime = {Config_dict['vegas.fold_dumptime']}\n"
+            output_str += f"vegas.fold_bins = {Config_dict['vegas.fold_bins']}\n"
+            output_str += f"vegas.fold_parfile = '{Config_dict['vegas.fold_parfile']}'\n"
+        
+        output_str += '"""'
+
+        if not valid_par:
+            output_str = output_str[:output_str.index('\n')]
+
+        self.Display_Script.append(output_str)
 
 
         def Generate_Command(Widget_Input, Widget_List):
@@ -301,9 +362,9 @@ class App(QMainWindow, Ui_MainWindow):
 
 
 
-        for Command_List_Index, Widget_Text in enumerate(Command_List):
-            print(Command_List)
-            Generate_Command(Widget_Text, Command_List_Index)
+        #for Command_List_Index, Widget_Text in enumerate(Command_List):
+        #    print(Command_List)
+        #    Generate_Command(Widget_Text, Command_List_Index)
             #print(Widget_Text, Command - List)
 
     #not sure what this does
